63ed772874dffe18a208d482aa9709097770b87d
denys <maxkuz28011@gmail.com>
2023-11-21 23:13:49 +0200

    HW1 - Done

---
+++ HW1/hw1.js
@@ -0,0 +1,65 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+let a1 = 'hello';
+console.log(a1)
+
+let a2 = 'owu';
+console.log(a2)
+
+let a3 = 'com';
+console.log(a3)
+
+let a4 = 'ua';
+console.log(a4)
+
+let a5 = 1;
+console.log(a5)
+
+let a6 = 10;
+console.log(a5)
+
+let a7 = -999;
+console.log(a7)
+
+let a8 = 123;
+console.log(a8)
+
+let a9 = 3.14;
+console.log(a9)
+
+let a10 = 2.7;
+console.log(a10)
+
+let a11 = 16;
+console.log(a11)
+
+let a12 = true;
+console.log(a12)
+
+let a13 = false;
+console.log(a13)
+
+//
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+
+let middleName = 'Kuz';
+let firstName = 'Maksym';
+let lastName = 'Ostapovich'
+
+let person=  middleName + ' ' +  firstName + ' ' + lastName ;
+console.log(person)
+//
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+let a = 100;
+console.log( typeof a)
+
+let b = '100';
+console.log(typeof b)
+
+let c = true;
+console.log(typeof c)
+
+
